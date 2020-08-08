737368bc8bfeadaca96f8ccd3bce271a4014bf58
doesnt matter@ <doesnt.matter@test.com>
2020-08-08 21:01:48 +0000

    WIP - incremental mods to get YHCR issued certs working on windows. Added windows traefik config (2019)

---
+++ ssl-test.js
@@ -0,0 +1,9 @@
+const https = require("https");
+const fs = require("fs");
+
+https.globalAgent.options.ca = [fs.readFileSync("./ssl/sandpit-ca.pem")];
+
+https.get("https://mfp.sandpit.yhcr.nhs.uk/fhir/stu3/Patient", ( res ) => {
+    console.log(res.statusCode);
+    console.log(res.headers);
+})
