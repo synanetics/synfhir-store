d89d4485f793fecf0bb7630bb4cddaf6846cff72
doesnt matter@ <doesnt.matter@test.com>
2020-08-10 12:47:17 +0000

    Windows Config done

--- ssl-test.js
+++ ssl-test.js
@@ -1,7 +1,7 @@
 const https = require("https");
 const fs = require("fs");
 
-https.globalAgent.options.ca = [fs.readFileSync("./ssl/sandpit-ca.pem")];
+https.globalAgent.options.ca = [fs.readFileSync("./ssl/certs/sandpit-ca.pem")];
 
 https.get("https://mfp.sandpit.yhcr.nhs.uk/fhir/stu3/Patient", ( res ) => {
     console.log(res.statusCode);
